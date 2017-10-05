012e3d0bf9d64248c6aea120edf8ae1ad53671eb
Alexis Low <alexisthelow@gmail.com>
2017-10-05 14:50:41 -0600

    added questions, methods to get random questions of different difficulty, method to check to see if answer was correct

--- src/challenges/question.java
+++ src/challenges/question.java
@@ -1,6 +1,72 @@
 
 package challenges;
 
-public class question {
+import java.util.Random;
 
+public class Question {
+
+	Random random = new Random();
+	
+	private String questionText;
+	private String answerText;
+	
+	private boolean answerCheck(String reply) {
+		if (this.getAnswerText().contains(reply)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public Question getEasyQuestion() {
+		return easy[random.nextInt(easy.length -1)];
+	}
+	
+	public Question getMediumQuestion() {
+		return medium[random.nextInt(medium.length -1)];
+	}
+	
+	public Question getHardQuestion() {
+		return hard[random.nextInt(hard.length -1)];
+	}
+	
+	public Question(String questionText, String answerText) {
+		this.questionText = questionText;
+		this.answerText = answerText;
+	}
+	
+	
+	private Question[] easy = {
+			new Question("True or false: we can compare an int variable with a boolean variable.", "false"),
+			new Question("What is the default value of a short?", "0"),
+			new Question("What is the extension of an uncompiled Java class?", "java"),
+			new Question("What is the default value of a boolean?", "false"),
+			new Question("True or false: \"=\" is the assignment operator.", "false"),
+			
+			
+			
+	};
+	
+	private Question[] medium = {
+			new Question("What is the size (in bytes) of a float?", "4"),
+			new Question("What is the size (in bytes) of a double?", "8"),
+			new Question("What is the method used to check if two Strings contain the same value?", "equals"),
+			
+	};
+	
+	private Question[] hard = {
+			new Question("What is the maximum value of an int?", "2147483647"),
+			new Question("What is the size (in bits) of a double?", "64"),
+			
+	};
+	
+	public String getQuestionText() {
+		return questionText;
+	}
+	
+	
+	public String getAnswerText() {
+		return answerText;
+	}
+	
+	
 }
